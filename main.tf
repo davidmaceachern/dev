ec75e20422d686f8f13b1b85cd64281f80dfe12c
David MacEachern <david.maceachern.tech@gmail.com>
2021-03-05 14:40:42 +0100

    Update require providers as per linting required in TF0.13+

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
-provider "aws" {
-  version = "~> 3.0"
-  region  = "eu-west-3"
-    endpoints {
-    sts = "https://sts.eu-west-3.amazonaws.com"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "dev-dmac"  
     key    = "default-infrastructure"
     region = "eu-west-3"
+  }  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-3"
+    endpoints {
+    sts = "https://sts.eu-west-3.amazonaws.com"
   }
 }
 
